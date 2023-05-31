f15ef1de220624d84bc2eff14a12e1032d2543cd
valeria caliguire <caliguireval@gmail.com>
2023-05-31 09:30:45 -0600

    core deliverables

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -1,17 +1,36 @@
 class NationalPark:
 
+    all = []
+
     def __init__(self, name):
         self.name = name
         self._trips = []
         self._visitors = []
+
+    @property
+    def national_park(self):
+        return self._national_park
+    
+    @national_park.setter
+    def national_park(self, national_park):
+        if isinstance(national_park, str) and not hasattr(self, "national_park"):
+            self._national_park = national_park
+        else:
+            raise Exception("National park cannot be change")
+
+
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if isinstance(new_trip,  Trip):
+            self._trip.append(new_trip)
+        return self._trip
     
     def visitors(self, new_visitor=None):
         from classes.visitor import Visitor
-        pass
+        if isinstance(new_visitor, Visitor):
+            self._visitor.append(new_visitor)
+        return self._visitor
     
     def total_visits(self):
         pass
--- lib/classes/visitor.py
+++ lib/classes/visitor.py
@@ -1,12 +1,32 @@
 class Visitor:
+    
+    all = []
+
 
     def __init__(self, name):
         self.name = name
+
+    @property
+    def visitor(self):
+        return self._visitor
+    
+    @visitor.setter
+    def visitor(self, visitor):
+        if isinstance(visitor, str) and 1 <= len(visitor) <= 15:
+            self._visitor = visitor
+        else:
+            raise Exception("Invalid visitor")
+    
+
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if isinstance(new_trip, Trip):
+            self._trip.append(new_trip)
+        return self._trip
     
     def national_parks(self, new_national_park=None):
         from classes.national_park import NationalPark
-        pass
+        if isinstance(new_national_park, NationalPark):
+            self._national_park.append(new_national_park)
+        return self._national_park
